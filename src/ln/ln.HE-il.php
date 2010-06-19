ec113778b68c52fee76690d95f9c5b069107819a
Keunhong Park <i@void.im>
2010-06-20 01:05:46 +0900

    add rest

---
+++ src/ln/ln.HE-il.php
@@ -0,0 +1,60 @@
+<?php
+if(!defined("_included")) exit;
+/***********************************************************************
+***	ln.HE-il language pack
+***********************************************************************/
+
+$ln->setvar("download","הורדה");
+
+$ln->setvar("select","בחירה אוטומטית");
+$ln->setvar("youtube","YouTube");
+$ln->setvar("googlevideo","Google Video");
+$ln->setvar("veoh","Veoh");
+$ln->setvar("mgoon","Mgoon");
+$ln->setvar("mncast","MNcast");
+$ln->setvar("pandoratv","Pandora TV");
+$ln->setvar("myspace","MySpace Video");
+$ln->setvar("spike","Spike");
+$ln->setvar("break_com","Break");
+$ln->setvar("putfile","PutFile");
+$ln->setvar("fivemin","5min");
+$ln->setvar("naver","Naver");
+$ln->setvar("daum","Daum tvPot");
+$ln->setvar("metacafe","MetaCafe");
+
+$ln->setvar("directions_direct","הורד סרטון מקישור אינטרנט");
+$ln->setvar("example","דוגמה");
+$ln->setvar("mgoon_warn","mgoon links are vaild only for a few seconds, if the link doesn't work, try again");
+
+$ln->setvar("result","result");
+$ln->setvar("download_link","click here to download the video");
+
+$ln->setvar("directions_search","Search");
+$ln->setvar("search","search");
+
+$ln->setvar("siteadd","Want a site added? Contact us at ");
+
+$ln->setvar("donate_descr","Please donate to keep this site running: ");
+
+
+/***********************************************************************
+***	search
+***********************************************************************/
+$ln->setvar("sort_by","Sort By");
+$ln->setvar("relevance","Relevance");
+$ln->setvar("view_count","View Count");
+$ln->setvar("date_added","Date Added");
+$ln->setvar("rating","Rating");
+
+$ln->setvar("all_time","All Time");
+$ln->setvar("today","Today");
+$ln->setvar("this_week","This Week");
+$ln->setvar("this_month","This Month");
+
+$ln->setvar("previous_page","previous page");
+$ln->setvar("next_page","next page");
+
+$ln->setvar("added","Added");
+$ln->setvar("from","From");
+$ln->setvar("views","Views");
+?>
---
+++ src/ln/ln.PL-pl.php
@@ -0,0 +1,59 @@
+<?php
+if(!defined("_included")) exit;
+/***********************************************************************
+***	ln.EN-uk language pack
+***********************************************************************/
+
+$ln->setvar("download","download");
+
+$ln->setvar("select","auto select");
+$ln->setvar("youtube","YouTube");
+$ln->setvar("googlevideo","Google Video");
+$ln->setvar("veoh","Veoh");
+$ln->setvar("mgoon","Mgoon");
+$ln->setvar("mncast","MNcast");
+$ln->setvar("pandoratv","Pandora TV");
+$ln->setvar("myspace","MySpace Video");
+$ln->setvar("spike","Spike");
+$ln->setvar("break_com","Break");
+$ln->setvar("putfile","PutFile");
+$ln->setvar("fivemin","5min");
+$ln->setvar("naver","Naver");
+$ln->setvar("daum","Daum tvPot");
+$ln->setvar("metacafe","MetaCafe");
+
+$ln->setvar("directions_direct","Download video from URL");
+$ln->setvar("example","Example");
+$ln->setvar("mgoon_warn","mgoon links are vaild only for a few seconds, if the link doesn't work, try again");
+
+$ln->setvar("result","result");
+$ln->setvar("download_link","click here to download the video");
+
+$ln->setvar("directions_search","Search");
+$ln->setvar("search","search");
+
+$ln->setvar("siteadd","Want a site added? Contact us at ");
+
+$ln->setvar("donate_descr","Please donate to keep this site running: ");
+
+/***********************************************************************
+***	search
+***********************************************************************/
+$ln->setvar("sort_by","Sort By");
+$ln->setvar("relevance","Relevance");
+$ln->setvar("view_count","View Count");
+$ln->setvar("date_added","Date Added");
+$ln->setvar("rating","Rating");
+
+$ln->setvar("all_time","All Time");
+$ln->setvar("today","Today");
+$ln->setvar("this_week","This Week");
+$ln->setvar("this_month","This Month");
+
+$ln->setvar("previous_page","previous page");
+$ln->setvar("next_page","next page");
+
+$ln->setvar("added","Added");
+$ln->setvar("from","From");
+$ln->setvar("views","Views");
+?>
